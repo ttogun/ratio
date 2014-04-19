4003d3f9764c5eb978d3f3c5b4d9621750818c9e
therealsonsy <mcsonusi@gmail.com>
2014-04-19 13:59:09 -0400

    acess classes

--- src/main/com/ratio/access/BuzzAccess.java
+++ src/main/com/ratio/access/BuzzAccess.java
@@ -1,7 +1,25 @@
 package main.com.ratio.access;
 
+import main.com.ratio.model.Buzz;
+
 /**
- * Created by user on 4/19/14.
+ * Created by Buzz on 4/19/14.
  */
 public class BuzzAccess {
+    //1. Get Buzz Inf
+    public Buzz getBuzzByEmail(String email) {
+        return null;
+    }
+
+    public void addBuzz(Buzz buzz) {
+
+    }
+
+    public void deleteBuzz(Buzz buzz) {
+
+    }
+
+    public void updateBuzzInfo(Buzz buzz) {
+
+    }
 }
---
+++ src/main/com/ratio/access/CheckInOutAccess.java
@@ -0,0 +1,26 @@
+package main.com.ratio.access;
+
+import main.com.ratio.model.CheckInOut;
+
+/**
+ * Created by user on 4/19/14.
+ */
+public class CheckInOutAccess {
+    public CheckInOut getCheckInOut() {
+        return null;
+    }
+
+
+    public void addCheckInOut(CheckInOut checkInOut) {
+
+    }
+
+    public void deleteCheckInOut(CheckInOut checkInOut) {
+
+    }
+
+    public void updateCheckInOut(CheckInOut checkInOut) {
+
+    }
+}
+
